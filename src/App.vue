b48341e38d8771360b31c456bff674812487c20e
lukepettway <luke.pettway@thinkbrownstone.com>
2020-12-19 17:53:58 -0500

    create basic flow for getting user location and connect to refugerestrooms api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <md-button class="md-primary md-raised" v-on:click="getBathroomsByLocation">Get safe bathrooms near me</md-button>
+    <ul id="example-1">
+      <li v-for="bathroom in bathrooms" :key="bathroom.id">
+        {{ bathroom.name }}
+      </li>
+    </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import axios from 'axios';
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+ 
+  },
+  data() {
+    return {
+      currentLocation: null,
+      bathrooms: [],
+    };
+  },
+  created() {
+    //this.getBathroomsByLocation();
+  },
+  methods: {
+    getPosition() {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      });
+    },
+     async getBathroomsByLocation(){
+      try {
+          const position = await this.getPosition();
+          this.currentLocation = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+          }   
+      } catch (err) {
+        console.log(err);
+      }
+
+      axios.get(`https://www.refugerestrooms.org/api/v1/restrooms/by_location`, {
+        params: {
+          lat: this.currentLocation.lat,
+          lng: this.currentLocation.lon
+        }
+      })
+      .then(response => {
+        this.bathrooms = response.data;
+        console.log(response.data);
+      })
+      .catch(e => {
+        this.errors.push(e)
+        console.log(e);
+      })
+    },
+  },
+  computed: {
+    computedIncidents() {
+      return this.bathrooms;
+    },
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
